1d9d7d2abad4d6a7ecec771849bc18156e5fcac3
haniffajri <hanif.fajri28@gmail.com>
2018-10-23 21:04:31 +0700

    edit array dengan format baru

--- variables/Array.js
+++ variables/Array.js
@@ -1,42 +1,44 @@
-const products = [
-    {
-        name: "Budi 4",
-        price: "Rp. 550.000",
-        category: "Action",
-        transaction: "25",
-        rating: 9
-    },
-    {
-        name: "GTA V",
-        price: "Rp. 650.000",
-        category: "RPG",
-        transaction:"134",
-        rating: 8
-    },
-    {
-        name: "FIFA 2019",
-        price: "Rp. 850.000",
-        category: "Sport",
-        transaction: "5",
-        rating: 7
-    }
-];
+// const products = [
+//     {
+//         name: "Budi 4",
+//         price: "Rp. 550.000",
+//         category: "Action",
+//         transaction: "25",
+//         rating: 9
+//     },
+//     {
+//         name: "GTA V",
+//         price: "Rp. 650.000",
+//         category: "RPG",
+//         transaction:"134",
+//         rating: 8
+//     },
+//     {
+//         name: "FIFA 2019",
+//         price: "Rp. 850.000",
+//         category: "Sport",
+//         transaction: "5",
+//         rating: 7
+//     }
+// ];
 
-// Iterasi/ Looping/ Perulangan
-// 1 Map
-// 2 for-Loop
+// // Iterasi/ Looping/ Perulangan
+// // 1 Map
+// // 2 for-Loop
 
-// Map
-products.map((dataObj, id) => {
-//    console.log(dataObj.name);
-//    console.log(dataObj.price);
-//    console.log(dataObj.rating); 
+// // Map
+// products.map((dataObj, id) => {
+// //    console.log(dataObj.name);
+// //    console.log(dataObj.price);
+// //    console.log(dataObj.rating); 
 
-   console.log(dataObj.name, dataObj.price, dataObj.rating);
-})
+//    console.log(dataObj.name, dataObj.price, dataObj.rating);
+// })
 
-console.log("==================================================================")
-// For-Loop
-for(let a=0; a < products.length ; a++){
-    console.log(products[a].name, products[a].price, products[a].rating)
-}
+// console.log("==================================================================")
+// // For-Loop
+// for(let a=0; a < products.length ; a++){
+//     console.log(products[a].name, products[a].price, products[a].rating)
+// }
+
+const arrayNew = [1,2,3,4]
